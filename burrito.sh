c5dba7b52d2cf6130845e1f791258260118a220f
Jason Reece <jason.reece@mac.com>
2014-08-16 22:05:49 -0400

    removed hard coded path to modules folder

--- burrito.sh
+++ burrito.sh
@@ -2,8 +2,6 @@
 
 MINPARAMS=1
 
-cd stylesheets/modules
-
 touch _$1.scss
 
 cap="$(tr '[:lower:]' '[:upper:]' <<< ${1:0:1})${1:1}"
@@ -141,5 +139,3 @@ for file in *.scss
 do
   echo '@import "'$file'";' | sed s/_// | sed s/.scss// | grep -v 'modules' | sort >> _modules.scss
 done
-
-cd ../../
