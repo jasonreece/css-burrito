d94c734743ae6b0adb09c1c0140e724536610423
Jason Reece <jason.reece@mac.com>
2014-08-16 22:41:35 -0400

    removes prompt from newburrito.sh

--- newburrito.sh
+++ newburrito.sh
@@ -4,4 +4,4 @@ git clone https://github.com/jasonreece/css-burrito.git
 
 mv css-burrito/* .
 
-rm -r LICENSE.md README.md burrito.sh css-burrito/ package.json setup.sh
+rm -r -f LICENSE.md README.md burrito.sh newburrito.sh css-burrito/ package.json setup.sh
